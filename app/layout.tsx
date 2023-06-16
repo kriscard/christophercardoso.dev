c5a0648f32c3552f01bde1e43ed915d35d604312
kriscard <cardoso.christopher01@gmail.com>
2023-06-16 03:47:37 +0000

    Setup Content layer and add placeholder blog posts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './global.css'
-import Header from '@components/header'
-import Footer from '@components/footer'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
