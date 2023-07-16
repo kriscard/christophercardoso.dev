bec59060d48dbcb1c78aee9ffa226fe30c705103
kriscard <cardoso.christopher01@gmail.com>
2023-07-16 03:59:24 +0000

    update banner description

--- lib/info.tsx
+++ lib/info.tsx
@@ -4,7 +4,7 @@ export const hi = () => <>Hey! I&apos;m Christopher</>
 
 export const description = () => (
   <>
-    I&apos;m a software developer based in Toronto with a passion for exploring and learning about
-    software development and architecture.
+    I&apos;m a software developer based in Toronto who loves exploring and learning about
+    software development while building cool projects.
   </>
 )
