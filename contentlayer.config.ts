2cd7fc34abc7a7a19df4aa92ad43e8fbdf4ca9f4
kriscard <cardoso.christopher01@gmail.com>
2023-08-10 20:03:31 +0000

    add new config for mdx

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,9 @@
 import { defineDocumentType, makeSource } from '@contentlayer/source-files'
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import remarkGfm from "remark-gfm"
+import { type Options } from "rehype-pretty-code"
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -15,4 +20,45 @@ export const Post = defineDocumentType(() => ({
   },
 }))
 
-export default makeSource({ contentDirPath: 'content', documentTypes: [Post] })
+
+const rehypePrettyCodeOptions: Partial<Options> = {
+  theme: "one-dark-pro",
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and allow empty
+    // lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }]
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node?.properties?.className?.push("line--highlighted")
+  },
+  onVisitHighlightedChars(node) {
+    node.properties.className = ["word--highlighted"]
+  },
+}
+
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        rehypePrettyCodeOptions,
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
+})
