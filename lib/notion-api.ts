a31f1fabe9448d005cda70a2fe1945e950f1dcb8
kriscard <cardoso.christopher01@gmail.com>
2023-08-14 01:13:34 +0000

    cleanup

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,7 +1,7 @@
 import { Client, isFullPage } from "@notionhq/client"
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY })
-const databaseId = `${process.env.NOTION_DATABASE_ID}`
+const databaseId = `${process.env.NOTION_DATABASE_API}`
 
 export const fetchProjects = async () => {
   try {
