73b19bdd1eb78814408005ed49f742226d8bd376
kriscard <contact@christophercardoso.dev>
2023-09-28 11:02:12 -0400

    Delete nvim lsp added text after save

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,3 @@
-
-[35m🌼 daisyUI 3.5.1[0m [0mhttps://daisyui.com
-╰╮
- ╰─ [32m✔︎[0m [2m[ [0m2[2m ][0m themes are enabled. You can add more themes or make your own theme:
-      https://daisyui.com/docs/themes
-
-[32m    ❤︎ Support daisyUI[0m: https://opencollective.com/daisyui[0m
-
 import React from "react"
 
 function AboutMeSection() {
