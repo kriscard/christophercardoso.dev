2334fceb27380ea8f6c43f5a385d850e4c52b608
kriscard <contact@christophercardoso.dev>
2024-08-09 20:48:25 -0400

    feat: revert change

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,8 @@ import { PresentationBanner } from "@/components/presentation-banner"
 import { ProjectsList } from "@/components/projects-list"
 
 export default async function HomePage() {
-  // const projects = (await fetchProjects()) as any as Project[] // Reset notion type which can always be different
+  const projects = (await fetchProjects()) as any as Project[] // Reset notion type which can always be different
 
-  const projects = []
   return (
     <section>
       <PresentationBanner />
